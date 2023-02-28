1eb9ff39d198e290317d822324f2c2c6482c2bdf
Euan Austin <elvismonarchy@proton.me>
2023-02-28 23:49:50 +0000

    Pseudo-Code

---
+++ main.py
@@ -0,0 +1,12 @@
+#PSEUDO CODE
+
+#scope -- simple
+#materials -- limited scope, no importing of packages/modules just the basic Python library
+#type -- CLI, not GUI or webapp
+#len -- less than 200 lines of code 
+
+#inputs -- add, modify, view, or delete expense (index number, type (fixed/one-off) data, payee, amount, description, mode, note )
+#outputs -- view segments of expenses (total by time period, payee, type,), comparison (fixed this month vs last) ... uses classes?
+#database- stores inputs, queried for outputs ... arrays and sub arrays?, need some key by which always identified like index? 
+
+#start with simplest version add amount and payee, returrn list of these and build from there 

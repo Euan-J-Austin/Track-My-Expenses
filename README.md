c4f8eea03a213bcef147c2fbba39028fe7ed0486
Euan Austin <elvismonarchy@proton.me>
2023-03-05 01:02:52 +0000

    Fully functional, need to add unique ID and read from txt

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Track-My-Expenses
-I  must start somewhere . . .
+
+My first Python project!
+
+The first of four 'simple' projects for my Python portfolio.
+
+Following the advice of Andy Sterkowitz, my simple project would use the basic Python library; use less than 200 lines of code; use the CLI as opposed to GUI or WebApp.
+
+## What does it do?
+
+Using the CLI, the user can navigate between the Main, Modification and Metric Menus. The Main menu allows users to print their total expenses in something approximating a tabular format (more on this later), exit the program and navigate to the other two menus. The Modification menu allows for the adding, deletion, changing of expense detail and return to the parent menu. The Metric menu gives information about total expenses, how much paid to a given payee and what proportion of total expenses this is, payments over a given amount. Each expense ID is unique, so they can be accessed for modification and deletion.
+
+ The user can also load or save the 'table' array where expenses are stored using the Python library's os module. Making use of the Object-Oriented Programming (OOP) paradigm, as opposed to say procedural code, the program allows efficient creation and use of interactive menus and the methods accessed through them. 
+
+## What doesn't it do?
+
+By definition, the scope of a simple project is limited. The printed table of all expenses is ugly, columns don't line-up. I could have used tkinter or tableau to resolve this. Aforementioned, my simple projects will use only the basic Python library. No unit-testing was performed, so amongst other things incorrect values could be passed such as string for the Sum. If this was a larger project I would make use of both.
--- main.py
+++ main.py
@@ -1,69 +1,72 @@
-#PSEUDO CODE
+# Make so can read table from a saved file without the formatting issues, unique IDs
 
-#scope -- simple
-#materials -- limited scope, no importing of packages/modules just the basic Python library
-#type -- CLI, not GUI or webapp
-#len -- less than 200 lines of code 
-
-#inputs -- add, modify, view, or delete expense (index number, type (fixed/one-off) data, payee, amount, description, mode, note )
-#outputs -- view segments of expenses (total by time period, payee, type,), comparison (fixed this month vs last) ... uses classes?
-#database- stores inputs, queried for outputs ... arrays and sub arrays?, need some key by which always identified like ID? 
-
-#to do: unique id, and print alignments, make mod menu actions a separate class
-
-
-table = [[1,17,'James'], [2,10,'Henry'],[3,3,'James']]
+import os 
 
 class modifyDb:
-    def __init__(self):
-        pass
-    def add_expense(self, ID, Amount, Payee):
-        return table.append([ID, Amount, Payee])
-    def del_expense(self, ID):
-        for x in table:
-          if x[0] == ID:
-            return table.remove(x)
-    def mod_expense(self, ID):
-        for x in table:
-          if x[0] == ID:
-            print(x)
-            usr_inp = input("Enter the feature you wish to modify e.g. 'Sum': ")
-            if usr_inp == 'Sum':
-              del[x[1]]
-              print(x)
-              new_sum = input("\nEnter a new sum: ")
-              x.insert(1, new_sum)
-              return print(f'\nSum modified:\n{x}')
-            elif usr_inp == 'Payee':
-              del[x[2]]
-              print(x)
-              new_payee = input("\nEnter a new payee: ")
-              x.insert(2, new_payee)
-              return print(f'\nPayee modified:\n{x}')
-              
-class measureDb():
+
   def __init__(self):
     pass
+
+  def add_expense(self, ID, Amount, Payee):
+    return table.append([ID, Amount, Payee])
+
+  def del_expense(self, ID):
+    for x in table:
+      if x[0] == ID:
+        return table.remove(x)
+
+  def mod_expense(self, ID):
+    for x in table:
+      if x[0] == ID:
+        print(x)
+        usr_inp = input("Enter the feature you wish to modify e.g. 'Sum': ")
+        if usr_inp == 'Sum':
+          del [x[1]]
+          print(x)
+          new_sum = input("\nEnter a new sum: ")
+          x.insert(1, new_sum)
+          return print(f'\nSum modified:\n{x}')
+        elif usr_inp == 'Payee':
+          del [x[2]]
+          print(x)
+          new_payee = input("\nEnter a new payee: ")
+          x.insert(2, new_payee)
+          return print(f'\nPayee modified:\n{x}')
+
+
+class measureDb():
+
+  def __init__(self):
+    pass
+
   def total_expenses(self):
-    total_expenses = int(sum([0 + int(x[1]) for x in table if x[1] != 'Sum']))
+    total_expenses = round(int(
+      sum([0 + int(x[1]) for x in table if x[1] != 'Sum'])),
+                           ndigits=2)
     print(f'\nYour total expenses are £{total_expenses}')
+
   def payee_stats(self):
-    total_expenses = int(sum([0 + int(x[1]) for x in table if x[1] != 'Sum']))
+    total_expenses = round(int(
+      sum([0 + int(x[1]) for x in table if x[1] != 'Sum'])),
+                           ndigits=2)
     payee_name = input("Enter the payee's name: ")
-    payee_total = sum([0 + x[1] for x in table if x[2] == payee_name])
-    payee_perc = round(int(payee_total)/total_expenses * 100, ndigits = 2)
-    print(f'\nYou paid {payee_name} £{payee_total}, this accounts for {payee_perc}% of your total expenses.')
+    payee_total = sum([0 + int(x[1]) for x in table if x[2] == payee_name])
+    payee_perc = round(int(payee_total) / total_expenses * 100, ndigits=2)
+    print(
+      f'\nYou paid {payee_name} £{payee_total}, this accounts for {payee_perc}% of your total expenses.'
+    )
+
   def over_amt(self):
     amt = int(input("\nEnter an amount: "))
     total_counter = 0
     for x in table:
       if int(x[1]) > amt:
         total_counter += 1
-      elif int(x[1]) > amt == 0:
-        print(f'\nYou have not paid out a sum greater than £{amt}')
     print(f'\nYou have {total_counter} transactions over £{amt}.')
 
+
 class navMenus(modifyDb, measureDb):
+
   def __init__(self):
     self.parent_menu_vis = """
 *** MAIN MENU ***
@@ -98,7 +101,8 @@ Type '3' to find total payments over a given amount.
 
 Type '4' to return to the main menu.\n
 """
-    self.end_message = '\nClosing Track-My-Expenses.'
+    self.end_message = '\nSaving expenses and closing Track-My-Expenses.'
+
   def parent_menu(self):
     print(self.parent_menu_vis)
     open = input()
@@ -110,17 +114,23 @@ Type '4' to return to the main menu.\n
       self.metric_menu()
     if open == "4":
       print(self.end_message)
+      saved_expenses.write(str(table))
+      saved_expenses.close()
       while 1 == 1:
         break
+
   def mod_menu(self):
     print(self.mod_menu_vis)
     self.mod_menu_actions()
+
   def mod_menu_actions(self):
     modify_inpt = input()
     if modify_inpt == "1":
-      add_exp = input("\nEnter an expense for addition in the 'ID, Amount, Payee' format e.g. 1, 17.00, James: ").split(',')
+      add_exp = input(
+        "\nEnter an expense for addition in the 'ID, Amount, Payee' format e.g. 1, 17.00, James: "
+      ).split(',')
       id = add_exp[0]
-      amount = round(float(add_exp[1]), ndigits = 2)
+      amount = round(float(add_exp[1]), ndigits=2)
       payee = add_exp[2]
       mdb.add_expense(id, amount, payee)
       print(f'\nYour expense has been added:\n{table}')
@@ -129,16 +139,17 @@ Type '4' to return to the main menu.\n
       del_exp = input("Enter ID of expense for deletion: ")
       ID = del_exp
       mdb.del_expense(ID)
-      print(f'Expense ID {del_exp} was deleted:\n{table}')
+      print(f'\nExpense ID {del_exp} was deleted:\n{table}')
       self.mod_menu()
     if modify_inpt == "3":
-      mod_exp = input("Enter ID of expense for modification: ")
-      ID = int(mod_exp)
+      mod_exp = input("\nEnter ID of expense for modification: ")
+      ID = mod_exp
       #this need to be passed as an integer for the function to work
       mdb.mod_expense(ID)
       self.mod_menu()
     if modify_inpt == "4":
       self.parent_menu()
+
   def metric_menu(self):
     print(self.metric_menu_vis)
     metric_input = input()
@@ -153,14 +164,20 @@ Type '4' to return to the main menu.\n
       self.metric_menu()
     if metric_input == '4':
       self.parent_menu()
+
   def print_db(self):
-    table.insert(0, ['ID', 'Sum', 'Payee'])
-    for row in table:
+    header_table = table.copy()
+    #copy table so not permanently modified with appended headers
+    header_table.insert(0, ['ID', 'Sum', 'Payee'])
+    for row in header_table:
       print(*row)
     self.parent_menu()
 
+table = []
+saved_expenses = open('track.txt', 'w')
 mdb = modifyDb()
 nm = navMenus()
-
 print("Welcome to Track-My-Expenses!\U0001F911\n")
 nm.parent_menu()
+
+
